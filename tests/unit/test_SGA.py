751d898785e33cd7992afab346a4cb5fe753f93e
Cezary Troska <CTroska@noblesys.com>
2021-01-25 09:04:21 +0100

    Testing switched to ddt

--- tests/unit/test_SGA.py
+++ tests/unit/test_SGA.py
@@ -1,26 +1,37 @@
+import itertools
 from unittest import TestCase
 
-from genetic.common import lambda_plus_mu
-from genetic.crossover import single_point_crossover
-from genetic.fitness import linear_fitness
+from ddt import ddt, data, unpack
+
+from genetic.common import lambda_plus_mu, lambda_coma_mu
+from genetic.crossover import single_point_crossover, double_point_crossover
+from genetic.fitness import linear_fitness, quadratic_fitness
 from genetic.ga_base import SGA
 from genetic.initial_pop import uniform_initial_population
 from genetic.mutation import reverse_bit_mutation
 from tests.test_common import get_default_set, default_termination_condition
 
 
+@ddt
 class TestSGA(TestCase):
 
-    def test_SGA_1(self):
-        result = SGA(
-            initial_population_generation=uniform_initial_population,
-            fitness_function=linear_fitness,
-            mutation_operator=reverse_bit_mutation,
-            crossover_operator=single_point_crossover,
-            sets=get_default_set(),
-            termination_condition=default_termination_condition,
-            population_merge_function=lambda_plus_mu
-        )
+    @data(*itertools.product([linear_fitness, quadratic_fitness], [single_point_crossover, double_point_crossover],
+                             [lambda_plus_mu, lambda_coma_mu], [10, 50, 100]))
+    @unpack
+    def test_SGA_1(self, fitness_function, crossover_operator, population_merge_function, iterations):
+        bug = False
+        try:
+            _ = SGA(
+                initial_population_generation=uniform_initial_population,
+                fitness_function=fitness_function,
+                mutation_operator=reverse_bit_mutation,
+                crossover_operator=crossover_operator,
+                sets=get_default_set(),
+                termination_condition=default_termination_condition,
+                population_merge_function=population_merge_function,
+                iterations=iterations
+            )
+        except:
+            bug = True
 
-        print(result)
-        self.assertTrue(True)
+        self.assertFalse(bug)
