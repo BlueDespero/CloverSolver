bcc373b74292f5bacde36bb22c3ab463b4c771a5
Cezary Troska <CTroska@noblesys.com>
2021-02-13 20:03:37 +0100

    Local variables shouldn't be changed every time

--- tests/test_effectiveness/run_tests.py
+++ tests/test_effectiveness/run_tests.py
@@ -81,16 +81,15 @@ def runbatch():
 
     algorithm = [SGA]
     initial_population_generation = [uniform_initial_population]
-    fitness_function = [quadratic_fitness, linear_fitness]
-    mutation_operator = [shuffle_column_mutation, shuffle_box_mutation, shuffle_row_mutation, reverse_bit_mutation]
-    mutation_rate = [0.1, 0.2, 0.3, 0.4, 0.5]
-    crossover_operator = [exchange_two_rows_crossover, exchange_two_columns_crossover, exchange_two_boxes_crossover,
-                          single_point_crossover, double_point_crossover]
+    fitness_function = [quadratic_fitness]
+    mutation_operator = [shuffle_column_mutation]
+    mutation_rate = [0.3, 0.5]
+    crossover_operator = [exchange_two_rows_crossover]
     termination_condition = [default_termination_condition]
-    population_merge_function = [lambda_plus_mu, lambda_coma_mu]
-    iterations = [101]
-    population_size = range(10, 310, 40)
-    number_of_children = range(10, 310, 40)
+    population_merge_function = [lambda_plus_mu]
+    iterations = range(2, 4)
+    population_size = range(10, 20, 10)
+    number_of_children = range(10, 20, 10)
 
     initial_state = []
     raport_batch = [manager.list()]
